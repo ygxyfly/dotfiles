fb4f92348825597643506077a9cc1dc9bacd1d89
Bo Fu <ygxyfly@gmail.com>
2024-05-27 14:56:22 -0700

    add more files

--- start_here/start_all_reps.sh
+++
@@ -1,7 +0,0 @@
-git clone git@github.com:ygxyfly/resume.git
-git clone git@github.com:ygxyfly/algorithm.git
-git clone git@github.com:ygxyfly/py.git
-git clone git@github.com:ygxyfly/pyLearn.git
-git clone git@github.com:ygxyfly/project.git
-git clone git@github.com:ygxyfly/bq.git
-git clone git@github.com:ygxyfly/ood.git
